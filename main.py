8acb472e357bf4552125c6bce61a4a93e0bd34cd
paulwc <pwchurch@gmail.com>
2014-05-12 11:46:36 -0400

    Changed file filter to accept more image formats

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import fnmatch
 
 directory = os.getcwd()
-pattern = '*.jpg'
+# pattern = '*.jpg'
 
 ident = {}
 files_dict = {}
@@ -11,8 +11,10 @@ match_count = 0
 
 # dump images into an array
 for root, dirs, files in os.walk(directory):
-    for filename in fnmatch.filter(files, pattern):
-        files_dict[filename] = os.path.join(root, filename)
+    # for filename in fnmatch.filter(files, pattern):
+    for filename in files:
+        if filename.endswith(('.jpg', '.jpeg', '.gif', '.png', '.tiff', '.tif')):
+            files_dict[filename] = os.path.join(root, filename)
 
 with open('omeka.csv') as f, open('omeka_with_images_no_spaces.csv', 'wb') as csvwritefile:
     csvreader = csv.DictReader(f)
